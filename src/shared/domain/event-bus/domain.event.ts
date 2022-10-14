75614bcd24e9c9bf92c129c585f672adfc2574a6
Edgar Castillo Vega <vayne.edgar271@gmail.com>
2022-10-14 18:13:15 -0500

    feat: new class's and interfaces were added to create new aggregate roots and entites

---
+++ src/shared/domain/event-bus/domain.event.ts
@@ -0,0 +1,32 @@
+import uuid from 'uuid'
+
+export abstract class DomainEvent {
+  static EVENT_NAME: string
+
+  static EVENT_TYPE: string | null
+
+  readonly eventName: string
+
+  readonly eventType: string | null
+
+  readonly eventId: string
+
+  readonly occurredAt: Date
+
+  constructor(eventName: string, eventType: string | null, eventId: string, occurredAt?: Date) {
+    this.eventName = eventName
+    this.eventType = eventType || null
+    this.eventId = eventId || uuid.v4()
+    this.occurredAt = occurredAt || new Date()
+  }
+
+  abstract toPrimitive(): Object
+
+  static fromPrimitives: (...args: any[]) => any
+}
+
+export type DomainEventClass = {
+  EVENT_NAME: string
+  EVENT_TYPE: string | null
+  fromPrimitives(...args: any[]): DomainEvent
+}
---
+++ src/shared/domain/event-bus/example.domain.event.ts
@@ -0,0 +1,58 @@
+import { DomainEvent } from "@/shared/domain/event-bus/domain.event";
+
+type CreateExampleDomainEventBody = {
+  readonly eventName: string;
+  readonly id: string;
+  readonly name: string;
+  readonly breed: string;
+};
+export class ExampleCreatedDomainEvent extends DomainEvent {
+  static readonly EVENT_NAME = "example.created";
+  static readonly EVENT_TYPE = null;
+
+  private id: string;
+  private name: string;
+  private breed: string;
+
+  constructor(id: string, name: string, breed: string, occurredAt: Date) {
+    super(
+      ExampleCreatedDomainEvent.EVENT_NAME,
+      ExampleCreatedDomainEvent.EVENT_TYPE,
+      id,
+      occurredAt
+    );
+    this.id = id;
+    this.name = name;
+    this.breed = breed;
+  }
+
+  getId(): string {
+    return this.id;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getBreed(): string {
+    return this.breed;
+  }
+
+  toPrimitive(): CreateExampleDomainEventBody {
+    const { id, name, breed } = this;
+    return {
+      id,
+      name,
+      breed,
+      eventName: ExampleCreatedDomainEvent.EVENT_NAME,
+    };
+  }
+
+  static fromPrimitives(
+    id: string,
+    body: CreateExampleDomainEventBody,
+    occurredAt: Date
+  ): DomainEvent {
+    return new ExampleCreatedDomainEvent(id, body.name, body.breed, occurredAt);
+  }
+}
