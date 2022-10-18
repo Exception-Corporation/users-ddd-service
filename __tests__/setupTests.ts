929321b5a1be547e93c1994477ea17d669e36416
Edgar Castillo Vega <vayne.edgar271@gmail.com>
2022-10-18 14:41:41 -0500

    fix: the correct prettier implementation all the ts files

--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -1,7 +1,7 @@
 //import { User } from "../src/user/v1/infrastructure/models/user.entity";
-import config from "../src/shared/infrastructure/config";
-import { modules } from "../src/index";
-import { PostgresDatabase } from "../src/shared/infrastructure/database/postgresql/postgres.database";
+import config from '../src/shared/infrastructure/config';
+import { modules } from '../src/index';
+import { PostgresDatabase } from '../src/shared/infrastructure/database/postgresql/postgres.database';
 
 // Initialize modules
 for (const Bootstrap of modules) {
